63dee30159f8e78a1b48eff26ad3a3b262464333
Sergi de la Fuente <serg.dlf@gmail.com>
2020-04-06 17:54:13 +0200

    Added basic map with geolocation

--- safewalks/src/app/heatmap/heatmap.page.ts
+++ safewalks/src/app/heatmap/heatmap.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-heatmap',
@@ -7,6 +9,31 @@ import { Component } from '@angular/core';
 })
 export class HeatmapPage {
 
-  constructor() { }
+  constructor(private geolocation: Geolocation) { }
+
+  map: Map;
+
+  ionViewDidEnter() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.leafletMap(resp.coords.latitude, resp.coords.longitude);
+    }).catch((error) => {
+      console.log('Error getting location', error);
+      // Default location
+      this.leafletMap(41.3870154, 2.1678531);
+    });
+  }
+
+  leafletMap(lat, long) {
+    this.map = new Map('map').setView([lat, long], 15);
+
+    tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+      attribution: 'edupala.com',
+    }).addTo(this.map);
+  }
+
+  /** Remove map when we have multiple map object */
+  ionViewWillLeave() {
+    this.map.remove();
+  }
 
 }
