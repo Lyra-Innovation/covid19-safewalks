38f868fd99728f97d957e398c925e3ecd37e4c5e
Erik Martinez <emrbeltane@gmail.com>
2020-04-08 17:35:56 +0200

    get_heat and max_heat

--- backend/api/config/map.php
+++ backend/api/config/map.php
@@ -1,4 +1,5 @@
 <?php
 
-$CNF["map_zoom"] = 19;
-$CNF["map_constant"] = pow(2, $CNF["map_zoom"]);
+$CNF['map_zoom'] = 19;
+$CNF['map_constant'] = pow(2, $CNF['map_zoom']);
+$CNF['map_max_heat'] = 10;
