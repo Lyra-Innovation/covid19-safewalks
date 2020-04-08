cfdfb5d9a83d76350fa81f9eb49e210c3f420ac6
Erik Martinez <emrbeltane@gmail.com>
2020-04-08 16:22:07 +0200

    Change objects to arrays

--- backend/api/app/Helpers/Cells.php
+++ backend/api/app/Helpers/Cells.php
@@ -4,13 +4,13 @@ namespace Safewalks\Helpers;
 
 class Cells {
 
-    static function pos_to_cell($lon, $lat) {
+    static function pos_to_cell($pos) {
         global $CNF;
 
-        $cell = new stdClass();
-        $cell->x = $CNF["map_constant"] * ($lon / 360.0 + 0.5);
-        $a = $lat * pi() / 180.0;
-        $cell->y = $CNF["map_constant"] * (1 - (log(tan($a) + 1.0/cos($a)) / pi())) * 0.5;
+        $cell = [];
+        $cell['x'] = $CNF["map_constant"] * ($pos['lon'] / 360.0 + 0.5);
+        $a = $pos['lat'] * pi() / 180.0;
+        $cell['y'] = $CNF["map_constant"] * (1 - (log(tan($a) + 1.0/cos($a)) / pi())) * 0.5;
 
         return $cell;
     }
@@ -18,10 +18,10 @@ class Cells {
     static function cell_to_pos($cell) {
         global $CNF;
 
-        $pos = new stdClass();
-        $pos->lon = $cell->x * 360.0 / $CNF["map_constant"] - 180.0;
-        $lat_rad = atan(sinh(pi() * (1.0 - 2.0 * $cell->y / $CNF["map_constant"])));
-        $pos->lat = $lat_rad * 180.0 / pi();
+        $pos = [];
+        $pos['lon'] = $cell['x'] * 360.0 / $CNF["map_constant"] - 180.0;
+        $lat_rad = atan(sinh(pi() * (1.0 - 2.0 * $cell['y'] / $CNF["map_constant"])));
+        $pos['lat'] = $lat_rad * 180.0 / pi();
 
         return $pos;
     }
