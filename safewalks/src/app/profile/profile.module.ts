e97eb55db664a475cf57982e5d96f889536a3ed0
Erik Martinez <emrbeltane@gmail.com>
2020-04-09 17:55:41 +0200

    Shared module and translations implemented

--- safewalks/src/app/profile/profile.module.ts
+++ safewalks/src/app/profile/profile.module.ts
@@ -1,18 +1,12 @@
-import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { ProfilePage } from './profile.page';
-import { TranslateModule } from '@ngx-translate/core';
+import { SharedModule } from '../shared.module';
 
 @NgModule({
   imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    RouterModule.forChild([{ path: '', component: ProfilePage }]),
-    TranslateModule
+    SharedModule,
+    RouterModule.forChild([{ path: '', component: ProfilePage }])
   ],
   declarations: [ProfilePage]
 })
