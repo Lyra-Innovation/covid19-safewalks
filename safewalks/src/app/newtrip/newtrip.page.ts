1bac2ac0f1bd020bfe6fc79db4d3d4a7036d16f6
Erik Martinez <emrbeltane@gmail.com>
2020-04-10 17:50:36 +0200

    Newtrip call to backend and alerts

--- safewalks/src/app/newtrip/newtrip.page.ts
+++ safewalks/src/app/newtrip/newtrip.page.ts
@@ -2,6 +2,9 @@ import { Component, ViewChild, OnInit, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import '@geoman-io/leaflet-geoman-free';
+import { ApiService } from '../services/api.service';
+import { AlertController } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-newtrip',
@@ -24,7 +27,10 @@ export class NewtripPage implements OnInit {
   }
 
   constructor(
-    private geolocation: Geolocation
+    private geolocation: Geolocation,
+    private api: ApiService,
+    public alertController: AlertController,
+    private translate: TranslateService,
   ) {}
 
   ngOnInit(): void {
@@ -52,6 +58,10 @@ export class NewtripPage implements OnInit {
     if (this.map != undefined) { this.map.remove(); }
     this.map = new L.Map('map_trip', {drawControl: true}).setView([lat, long], 15);
     this.map.invalidateSize();
+    this.map.coords = [];
+    this.map.on('pm:create', function(e) {
+      this.coords = this.coords.concat(e.layer.getLatLngs());
+    });
 
     L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
       attribution: 'edupala.com',
@@ -72,7 +82,82 @@ export class NewtripPage implements OnInit {
       pinningOption: false,
       snappingOption: false
     });
-
   }
-  
+
+  validate() {
+    var ret = [];
+    ret['success'] = false;
+    if (!this.selectedDate) {
+      ret['error'] = 'nodate';
+    } else if (this.map.coords.length == 0) {
+      ret['error'] = 'nocoords';
+    } else {
+      ret['success'] = true;
+    }
+
+    return ret;
+  }
+
+  send_trip() {
+    var validation = this.validate();
+
+    if (!validation['success']) {
+      this.alertValidation(validation['error']);
+      return;
+    }
+
+    var points = [];
+    for (var i = 0; i < this.map.coords.length; i++) {
+      points.push({
+        "lat": this.map.coords[i].lat,
+        "lon": this.map.coords[i].lng
+      });
+    }
+    this.api.post('Trip', 'createTrip', {
+      "start_date": Math.floor(new Date(this.selectedDate).getTime() / 1000),
+      "enforced": false,
+      "vehicle": this.speed_selected,
+      "points": points
+    }).subscribe({
+      next: (resp: {data: any}) => {
+        if (resp.data.inserted) {
+          this.alertSuccess();
+        } else {
+          // show popup with resp.data.alternative
+          console.log(resp.data.alternative);
+        }
+      },
+      error: error => {
+        console.error('There was an error!', error);
+      }
+    });
+  }
+
+  async alertValidation(message) {
+    const alert = await this.alertController.create({
+      header: this.translate.instant('newtrip.alert_validation_header'),
+      message: this.translate.instant('newtrip.' + message),
+      buttons: [
+        {
+          text: this.translate.instant('newtrip.ok'),
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async alertSuccess() {
+    const alert = await this.alertController.create({
+      header: this.translate.instant('newtrip.alert_success_header'),
+      message: this.translate.instant('newtrip.success'),
+      buttons: [
+        {
+          text: this.translate.instant('newtrip.ok'),
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
